7cf671d652d0b63214cf0761a2fae96ccfa7d916
Ashu-Bashir <ashu.bashir@anywhere.co>
2023-02-02 12:32:57 +0530

    mentioned what a shallow copy is in comments

--- deepVsShallowCloningObj.js
+++ deepVsShallowCloningObj.js
@@ -1,17 +1,17 @@
 const arr = [1, 2, 3, { name: "mukesh" }];
 
 const answer = arr.map((v) => {
-  if (v.constructor.name === "Number") {
-    return v * 2;
-  }
-   else if (v.constructor.name === "Object") {
-    //  const deepClone= JSON.parse(JSON.stringify(v));
-    const shallowCopy = { ...v };
-
-    const objKey = Object.getOwnPropertyNames(v);
-
-    const firstKey = objKey[0];
-    shallowCopy[firstKey] = "suresh";
-    return shallowCopy;
-  }
-});
+    if (v.constructor.name === "Number") {
+        return v * 2;
+      }
+       else if (v.constructor.name === "Object") {
+        //  const deepClone= JSON.parse(JSON.stringify(v));
+        const shallowCopy = { ...v }; // shallow cloning,. use destructuring using rest operator
+    
+        const objKey = Object.getOwnPropertyNames(v);
+    
+        const firstKey = objKey[0];
+        shallowCopy[firstKey] = "suresh";
+        return shallowCopy;
+      }
+    });
